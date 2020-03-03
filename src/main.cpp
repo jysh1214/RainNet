41e8d3d527dd71fbac9b41a9c8b0170654b1cf2b
jysh1214 <jyxemperor@gmail.com>
2020-03-04 00:49:22 +0800

    2020_03_04

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "../include/rain_net.h"
 
-Layer* layer_0 = new ConnectedLayer(1, "sigmoid");
+Layer* layer_0 = new ConnectedLayer(2, "sigmoid");
+Layer* layer_1 = new ConnectedLayer(2, "sigmoid");
+Layer* layer_2 = new ConnectedLayer(1, "sigmoid");
 
 int main()
 {
     Net net;
     net.layers.push_back(layer_0);
+    net.layers.push_back(layer_1);
+    net.layers.push_back(layer_2);
     return 0;
 }
